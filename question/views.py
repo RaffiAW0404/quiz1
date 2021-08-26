dc4186eccf02078cfcae5d39c4365897a00c2d02
RaffiAW0404 <83364826+RaffiAW0404@users.noreply.github.com>
2021-08-26 16:09:15 +0100

    did the programming

--- question/views.py
+++ question/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "question/index.html")
+
+def cc1(request):
+    return render(request, "question/cc1.html")
+
+def qz1001(request):
+    return render(request, "question/qz1001.html")
